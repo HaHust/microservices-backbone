7e5d260a13f3e38ca5c0b397df4483bddbfd0248
HaHust <vanha.hust@gmail.com>
2021-10-13 20:51:04 +0700

    * create eureka server

---
+++ eureka-server/src/test/java/com/h2s/demo/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h2s.demo.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
